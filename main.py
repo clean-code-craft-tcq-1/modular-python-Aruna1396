26325508e8a2b18725d1dc07081e1321845b0672
Aruna1396 <77557658+Aruna1396@users.noreply.github.com>
2021-02-23 14:37:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import color_coder_test as test
-import reference_manual_generator as manual
+from color_coder_test import execute_unittests
+from reference_manual_generator import create_reference_manual
 
 if __name__ == '__main__':
-    test.execute_unittests()
-    reference_manual = manual.create_reference_manual()
+    execute_unittests()
+    reference_manual = create_reference_manual()
     print("\n Reference Manual for Telecomm Insulation Wire color codes \n", reference_manual)
     print('Done :)')

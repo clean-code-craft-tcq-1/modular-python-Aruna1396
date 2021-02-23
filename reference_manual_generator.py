4419ee3c288218120eed4184e29a5d3861b0dcfc
Aruna1396 <77557658+Aruna1396@users.noreply.github.com>
2021-02-23 23:46:42 +0530

    Remove PrettyTable external library

--- reference_manual_generator.py
+++ reference_manual_generator.py
@@ -1,20 +1,17 @@
 from colorpair_generator import get_color_from_pairnumber
 from cable_colors import get_colorpair_names
-from prettytable import PrettyTable
 
 """
-This function creates a reference manual with color pair names
-and color codes in a tabular format. External Library is
-used for formatting purposes to improve readability.
+This function is implemented to create a reference manual with 
+color names and its corresponding codes for the wiring personnel
 """
 
 
-def create_reference_manual():
+def print_reference_manual():
     pair_number_start_index = 1
     pair_number_end_index = 26
-    reference_manual = PrettyTable(['Pair Number', 'Color Pairs'])
+    print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
     for pair_number in range(pair_number_start_index, pair_number_end_index):
-        color_pair = get_color_from_pairnumber(pair_number)
-        formatted_colorpair = get_colorpair_names(color_pair[0], color_pair[1])
-        reference_manual.add_row([pair_number, formatted_colorpair])
-    return reference_manual
+        major_color, minor_color = get_color_from_pairnumber(pair_number)
+        formatted_colorpair = get_colorpair_names(major_color, minor_color)
+        print("{:<20} {:<15}".format(pair_number, formatted_colorpair))

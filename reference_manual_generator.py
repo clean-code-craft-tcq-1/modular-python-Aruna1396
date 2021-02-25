c5b08c50cd6c517841fb161be0e2f89dc6be4eb8
Aruna1396 <77557658+Aruna1396@users.noreply.github.com>
2021-02-25 18:45:50 +0530

    Update reference_manual_generator.py

--- reference_manual_generator.py
+++ reference_manual_generator.py
@@ -9,7 +9,7 @@ color names and its corresponding codes for the wiring personnel
 
 def print_reference_manual():
     pair_number_start_index = 1
-    pair_number_end_index = len(color.MAJOR_COLORS) * len(color.MAJOR_COLORS) + 1
+    pair_number_end_index = len(color.MAJOR_COLORS) * len(color.MINOR_COLORS) + 1
     print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
     for pair_number in range(pair_number_start_index, pair_number_end_index):
         major_color, minor_color = color.get_color_from_pairnumber(pair_number)

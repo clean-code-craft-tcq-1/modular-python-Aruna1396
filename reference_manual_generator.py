f7388d49c3e4ab6331d5533db708729b66339ec9
Aruna1396 <77557658+Aruna1396@users.noreply.github.com>
2021-02-25 18:39:57 +0530

    Remove hardcoded value for pair_number_end_index

--- reference_manual_generator.py
+++ reference_manual_generator.py
@@ -1,4 +1,4 @@
-from colorpair_generator import get_color_from_pairnumber
+import colorpair_generator as color
 from cable_colors import get_colorpair_names
 
 """
@@ -9,9 +9,9 @@ color names and its corresponding codes for the wiring personnel
 
 def print_reference_manual():
     pair_number_start_index = 1
-    pair_number_end_index = 26
+    pair_number_end_index = len(color.MAJOR_COLORS) * len(color.MAJOR_COLORS) + 1
     print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
     for pair_number in range(pair_number_start_index, pair_number_end_index):
-        major_color, minor_color = get_color_from_pairnumber(pair_number)
+        major_color, minor_color = color.get_color_from_pairnumber(pair_number)
         formatted_colorpair = get_colorpair_names(major_color, minor_color)
         print("{:<20} {:<15}".format(pair_number, formatted_colorpair))

0a09570e01205fa6616c4ea6af0a1ea600d9838f
Aruna Gopalakrishnan <Gopalakrishnan.Aruna@in.bosch.com>
2021-02-27 23:16:27 +0530

    Fixed the review comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from color_coder_test import execute_unittests
-from reference_manual_generator import print_reference_manual
+from reference_manual_generator import print_reference_manual, create_reference_manual
 
 if __name__ == '__main__':
     execute_unittests()
-    print("\n Reference Manual for Telecomm Insulation Wire color codes \n",)
-    print_reference_manual()
+    print("\n Reference Manual for Telecomm Insulation Wire color codes \n", )
+    reference_manual = create_reference_manual()
+    print_reference_manual(reference_manual)
     print('Done :)')
--- reference_manual_generator.py
+++ reference_manual_generator.py
@@ -7,11 +7,18 @@ color names and its corresponding codes for the wiring personnel
 """
 
 
-def print_reference_manual():
+def create_reference_manual():
+    reference_manual = []
     pair_number_start_index = 1
     pair_number_end_index = len(color.MAJOR_COLORS) * len(color.MINOR_COLORS) + 1
-    print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
     for pair_number in range(pair_number_start_index, pair_number_end_index):
         major_color, minor_color = color.get_color_from_pairnumber(pair_number)
         formatted_colorpair = get_colorpair_names(major_color, minor_color)
-        print("{:<20} {:<15}".format(pair_number, formatted_colorpair))
+        reference_manual.append("{:<15} {:<10} ".format(pair_number, formatted_colorpair))
+    return reference_manual
+
+
+def print_reference_manual(reference_manual):
+    print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
+    for manual_entries in reference_manual:
+        print(manual_entries)
